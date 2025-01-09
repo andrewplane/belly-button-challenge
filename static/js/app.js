b9f8dd90fa07c7d1bfd02f9dbf7c51ee562ce6da
andrewplane <andrew@lanemultimedia.com>
2025-01-09 10:31:10 -0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -29,34 +29,74 @@ function buildMetadata(sample) {
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    console.log(data);
-    console.log(sample);
+
     // Get the samples field
-    let samplesField = data.sample;
-    console.log(`samplesField: `, samplesField);
+    let samplesField = data.samples;
 
     // Filter the samples for the object with the desired sample number
-    let filteredData = samplesField.filter(item => item.id === '940');
+    let filteredData = samplesField.filter(item => item.id === sample);
     let sampleData = filteredData[0];
     console.log(`sampleData`, sampleData);
 
     // Get the otu_ids, otu_labels, and sample_values
+    let otu_ids = sampleData.otu_ids;
+    let otu_labels = sampleData.otu_labels;
+    let sample_values = sampleData.sample_values;
 
+    // console.log('Check: ', sampleValues);
 
     // Build a Bubble Chart
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      marker: {
+        sizes: sample_values/43,
+        color: otu_ids,
+      },
+      text: otu_labels,
+      type: 'scatter'
+    };
 
+    let bubbleData = [trace1];
+
+    let layout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {title: 'OTU ID'},
+      yaxis: {title: 'Number of Bacteria'}
+    }
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    // Sort the data by sample_values descending
+    let sortedSamples = samplesField.sort((a, b) => b.sample_values - a.sample_values);
 
+    // Slice the first 10 objects for plotting
+    let firstTenSamples = sortedSamples.slice(0, 10);
+    console.log(`firstTenSamples`, firstTenSamples);
+
+    // Reverse the array to accommodate Plotly's defaults
+    firstTenSamples.reverse();
+
+    // Trace1 for the Greek Data
+    let trace2 = {
+      x: firstTenSamples.map(object => object.sample_values),
+      y: slicedData.map(object => object.greekName),
+      text: slicedData.map(object => object.greekName),
+      name: "Greek",
+      type: "bar",
+      orientation: "h"
+    };
+    
+    let yticks = otu_ids.map(id => `OTU ${id}`);
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
 
-
     // Render the Bar Chart
+    Plotly.newPlot("bar", barData, layout)
 
   });
 }
@@ -69,7 +109,13 @@ function init() {
     let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-    let dropdown = d3.select('#selDataset');
+    const dropdown = d3.select('#selDataset');
+    let selectedValue = [];
+    // dropdown.addEventListener('change', function(event) {
+    //   const selectedValue = event.target.value;
+    // });
+
+
 
     // Use the list of sample names to populate the select options
     dropdown.selectAll('option')  // Select all 'option' elements  
@@ -78,21 +124,29 @@ function init() {
       .append('option')           // Append the '<option>' tag
       .text(d => d)               // Set text to the element in names
       .attr('value', d => d);     // Set value of each to the element in names
-
+      
+    d3.select('#selDataset')
+      .on('change', function(event) {
+        selectedValue = d3.select(this).property('value');
+        console.log('Selected Value: ', selectedValue);
+    });
     // Get the first sample from the list
-    let firstSample = names[0];   
-    
+    // let firstSample = names[0];   
+    // let sample = dropdown.property('value');
+    // sample = selectedValue;
+    // console.log(`sample`, sample);
     // Build charts and metadata panel with the first sample
-    buildCharts(firstSample);  
-    buildMetadata(firstSample*1); // Convert firstSample to a number
+    buildCharts(selectedValue);  
+    buildMetadata(selectedValue*1); // Convert firstSample to a number
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-  buildCharts(firstSample);
-  buildMetadata(firstSample);
+  // sample = dropdown.property('value');
+  // buildCharts(sample);
+  // buildMetadata(sample*1);
 }
 
 // Initialize the dashboard
