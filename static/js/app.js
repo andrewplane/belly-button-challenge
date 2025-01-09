f6cac343934258471dbb7a23954b43deef2ba1c0
andrewplane <andrew@lanemultimedia.com>
2025-01-08 16:29:10 -0800

    javascript update

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,25 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
-
+    let filteredData = metadata.filter(item => item.id === sample);
+    let sampleMetaData = filteredData[0];
+    
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let demographics = d3.select(`#sample-metadata`);
 
     // Use `.html("") to clear any existing metadata
+    demographics.html("");
 
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
+    // Iterate over the sample metadata for each key-value in the filtered metadata.
+    Object.entries(sampleMetaData).forEach(([key, value]) => {
+      demographics.append('span')        // New paragraph for each field
+      .text(`${key.toUpperCase()}: ${value}`);      // add text "field: value"
+    
+      demographics.append('br');
+    })
 
   });
 }
@@ -23,12 +29,16 @@ function buildMetadata(sample) {
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
+    console.log(data);
+    console.log(sample);
     // Get the samples field
-
+    let samplesField = data.sample;
+    console.log(`samplesField: `, samplesField);
 
     // Filter the samples for the object with the desired sample number
-
+    let filteredData = samplesField.filter(item => item.id === '940');
+    let sampleData = filteredData[0];
+    console.log(`sampleData`, sampleData);
 
     // Get the otu_ids, otu_labels, and sample_values
 
@@ -56,28 +66,33 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select('#selDataset');
 
     // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-
+    dropdown.selectAll('option')  // Select all 'option' elements  
+      .data(names)                // Link with data in 'namea' array
+      .enter()                    // Create new 'option' for each new item
+      .append('option')           // Append the '<option>' tag
+      .text(d => d)               // Set text to the element in names
+      .attr('value', d => d);     // Set value of each to the element in names
 
     // Get the first sample from the list
-
-
+    let firstSample = names[0];   
+    
     // Build charts and metadata panel with the first sample
-
+    buildCharts(firstSample);  
+    buildMetadata(firstSample*1); // Convert firstSample to a number
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(firstSample);
+  buildMetadata(firstSample);
 }
 
 // Initialize the dashboard
